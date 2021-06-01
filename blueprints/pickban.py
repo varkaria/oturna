5868320007080e909a83ca6a155758676d500c41
Gusbell <77166960+Gusb3ll@users.noreply.github.com>
2021-06-01 23:33:03 +0700

    add: state display system (very noob one)

--- blueprints/pickban.py
+++ blueprints/pickban.py
@@ -4,4 +4,4 @@ pickban = Blueprint('pickban', __name__)
 
 @pickban.route('/')
 def index():
-    return render_template('pickban/pickban.html')
+    return render_template('pickban/pickban.html', team_name="'Test'", state='Ban')
