e1e7552fef791ca33cd38b2fa7c4677be1186291
Gusbell <77166960+Gusb3ll@users.noreply.github.com>
2021-06-02 16:30:26 +0700

    remove: unwanted stuff

--- blueprints/pickban.py
+++ blueprints/pickban.py
@@ -5,10 +5,9 @@ db = mysql.DB()
 
 pickban = Blueprint('pickban', __name__)
 
-@pickban.route('/<match_id>')
-def index(pool_id=db.current_round['id'], match_id=db.current_round['id']):
+@pickban.route('/')
+def index(pool_id=db.current_round['id']):
     mappool = db.get_mappool(pool_id)
-    match_id = db.get_matchs(match_id) #IDK if this is correct but my head exploded
     if request.args.get('json'):
         return jsonify(mappool) 
-    return render_template('pickban/pickban.html', team1='Varkaria', team2='Gusbell', team_choose='Varkaria', state='Ban', match_id=match_id, mappool=mappool, pool_id=pool_id)
+    return render_template('pickban/pickban.html', team1='Varkaria', team2='Gusbell', team_choose='Varkaria', state='Ban', mappool=mappool, pool_id=pool_id)
