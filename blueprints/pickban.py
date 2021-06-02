fcf6cc07c220e19a1368d238dc8a3842250b8c18
Gusbell <77166960+Gusb3ll@users.noreply.github.com>
2021-06-02 14:01:57 +0700

    add: make pickban pull data from sql

--- blueprints/pickban.py
+++ blueprints/pickban.py
@@ -1,7 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
+from objects import mysql
+
+db = mysql.DB()
 
 pickban = Blueprint('pickban', __name__)
 
-@pickban.route('/')
-def index():
-    return render_template('pickban/pickban.html', team_name="'Test'", state='Ban')
+@pickban.route('/<match_id>')
+def index(pool_id=db.current_round['id'], match_id=db.current_round['id']):
+    mappool = db.get_mappool(pool_id)
+    match_id = db.get_matchs(match_id) #IDK if this is correct but my head exploded
+    if request.args.get('json'):
+        return jsonify(mappool) 
+    return render_template('pickban/pickban.html', team1='Varkaria', team2='Gusbell', team_choose='Varkaria', state='Ban', match_id=match_id, mappool=mappool, pool_id=pool_id)
