a2b6e49be7b30b3ec8263d9e03246712d60a9ab9
Pavaresorn Malai <61514399+Varkaria@users.noreply.github.com>
2021-06-14 00:33:13 +0700

    :white_check_mark: [stream] new mappool showcase layout

--- static/js/stream/showcase.js
+++ static/js/stream/showcase.js
@@ -24,30 +24,19 @@ socket.onmessage = event => {
 	let data = JSON.parse(event.data),
         map = data.menu.bm;
 
-	if (data.menu.state == 2) {
+	if (data.menu.state == 7) {
         if (tempState !== data.menu.state) {
             tempState = data.menu.state
-            document.querySelector('#songs').innerHTML += `
-            <div class="song pre-animation" id="${Math.random().toString(36).substring(7)}">
-                <div class="l-side">
-                    <div class="song-meta">
-                        <div class="artist">${map.metadata.artist}</div>
-                        <div class="title">${map.metadata.title}</div>
-                    </div>
-                    <div class="song-stats">Star : <b>${map.stats.fullSR}</b> OD : <b>${map.stats.memoryOD}</b></div>
-                </div>
-                <div class="r-side">
-                    <div class="stage">Stage ${stage}</div>
-                </div>
-                <div class="map-darker"></div>
-                <img src="http://127.0.0.1:24050/Songs/${data.menu.bm.path.full}?a=${Math.random(10000)}" class="map-bg">
-            </div>
-            `
-            setTimeout(function(){
-                document.querySelector('.pre-animation').classList.remove('pre-animation')
-            },1500)
-
-            stage = stage + 1
+            document.getElementById("bimg").src = `http://127.0.0.1:24050/Songs/${data.menu.bm.path.full}?a=${Math.random(10000)}`;
+            document.getElementById("title").innerHTML = map.metadata.title
+            document.getElementById("artist").innerHTML = map.metadata.artist
+            document.getElementById("mapper").innerHTML = map.metadata.mapper
+            document.getElementById("diff").innerHTML = map.metadata.difficulty
+            document.getElementById("star").innerHTML = map.stats.fullSR
+            document.getElementById("od").innerHTML = map.stats.memoryOD
+            document.getElementById("hp").innerHTML = map.stats.memoryHP
+            document.getElementById("bpm").innerHTML = map.stats.bpm.max
+            document.getElementById("removeme").classList.remove("remove-me");
         }
     } else {
         if (tempState !== data.menu.state) {
