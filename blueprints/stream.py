c1544646fe846d1104d539a9384caacb2b1b2f09
Gusbell <77166960+Gusb3ll@users.noreply.github.com>
2021-06-14 14:39:52 +0700

    add: make every file downloaded name 'brackets.json'

--- blueprints/stream.py
+++ blueprints/stream.py
@@ -1,7 +1,6 @@
 import os, datetime
-from flask import Blueprint, render_template, request, session, current_app, send_from_directory, Response
+from flask import Blueprint, render_template, request, session, current_app, Response, send_file
 from objects import mysql
-from werkzeug.utils import secure_filename
 
 db = mysql.DB()
 stream = Blueprint('stream', __name__)
@@ -61,31 +60,47 @@ def json_upload():
     if request.method == 'POST':
         file = request.files['file']
         if file.filename == '':
-            return 'No selected file'
+            error = 'No file selected'
+            return render_template('stream/json_upload.html', error=error)
         if file and allowed_file(file.filename):
-            now = datetime.datetime.now()
-            id = str(session['user_id'])
-            date_time = now.strftime("%d-%m-%Y")
-            filename = '[' + id + ']' + ' ' + date_time + '.json'
-            file.save(os.path.join(BRACKETS_FOLDER, filename))
+            try:
+                now = datetime.datetime.now()
+                id = str(session['user_id'])
+                date_time = now.strftime("%d-%m-%Y")
+                filename = '[' + id + ']' + ' ' + date_time + '.json'
+                file.save(os.path.join(BRACKETS_FOLDER, filename))
+                error = 'File upload successfully'
+                return render_template('stream/json_upload.html', error=error)
+            except KeyError:
+                error = 'You need to login to upload this file'
+                return render_template('stream/json_upload.html', error=error)
         else:
-            return 'Invalid file type'
-        return 'file uploaded successfully'
+            error = 'Invalid file type'
+            return render_template('stream/json_upload.html', error=error)
     return render_template('stream/json_upload.html')
 
 @stream.route('/json/download/', methods=['GET', 'POST'])
 def download_file():
     path = BRACKETS_FOLDER
     list_brackets = {}
-    if os.listdir(path) != []:
-        for filename in os.listdir(path):
-            list_brackets[filename] = filename
-            print(filename)
-        return render_template('stream/json_download.html', filename=filename, list_brackets=list_brackets)
-    else:
-        return 'No json uploaded'
+    try:
+        id = str(session['user_id'])
+        if os.listdir(path) != []:
+            for filename in os.listdir(path):
+                list_brackets[filename] = filename
+                print(filename)
+            return render_template('stream/json_download.html', filename=filename, list_brackets=list_brackets)
+        else:
+            error = 'No json uploaded'
+            return render_template('stream/json_download.html', error=error)
+    except KeyError:
+        error = 'You need to login to view the file'
+        return render_template('stream/json_download.html', error=error)
     
 @stream.route('/json/download/<path:filename>', methods=['GET', 'POST'])
 def download(filename):
-    file = os.path.join(current_app.root_path, BRACKETS_FOLDER) + filename
-    return Response(file, mimetype='application/json', headers={'Content-Disposition': 'attachment;filename=zones.geojson'})
+    file = os.path.join(current_app.root_path, BRACKETS_FOLDER) + '/' +filename
+    response = send_file(file, mimetype='application/json',attachment_filename='brackets.json', as_attachment=True)
+    response.headers["x-filename"] = 'brackets.json'
+    response.headers["Access-Control-Expose-Headers"] = 'x-filename'
+    return response
