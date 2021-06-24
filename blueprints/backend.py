3d0d986d9b738b4920335b7d60bd6acfefb88826
Pavaresorn Malai <61514399+Varkaria@users.noreply.github.com>
2021-06-25 02:02:33 +0700

    :white_check_mark: progression of preduction

--- blueprints/backend.py
+++ blueprints/backend.py
@@ -219,6 +219,27 @@ def match_update(id):
         flash(e, 'danger')
         return redirect(url_for('backend.matchs'))
 
+@backend.route('/schedule/match/<id>/preduct', methods=['POST'])
+@login_required
+@need_privilege(Staff.COMMENTATOR)
+def match_preduct_update(id):
+    try:
+        w = request.form['win']
+        l = request.form['lose']
+        s = request.form['man']
+        
+        # fak = db.query_one(f"SELECT id, team1, team2 FROM match WHERE id={id}")
+        # if s == 'team1':
+        #     db.query_one("INSERT INTO `tourney`.`com_preducts` (`match_id`, `commentator`, `s_win`, `s_team1`, `s_team2`) VALUES (%s, %s, %s, %s, %s);", (id,session['id'],res[f'team{s}'],2,l))
+        # else:
+        #     db.query_one("INSERT INTO `tourney`.`com_preducts` (`match_id`, `commentator`, `s_win`, `s_team1`, `s_team2`) VALUES (%s, %s, %s, %s, %s);", (id,session['id'],res[f'team{s}'],l,2))
+        
+        flash('s', 'success')
+        return redirect(url_for('backend.matchs'))
+    except Exception as e:
+        flash(e, 'danger')
+        return redirect(url_for('backend.matchs'))
+
 @backend.route('/schedule/match/<id>/delete', methods=['POST'])
 @login_required
 @need_privilege(Staff.ADMIN)
